a386a8aa0332d196dfaa55ac65f5c66063f05eff
Roxana <petobirlea.1934897@studenti.uniroma1.it>
2023-06-23 19:38:56 +0200

    fix mouth

--- main_gui.py
+++ main_gui.py
@@ -9,6 +9,8 @@ import ttkbootstrap as ttk
 # ---------- GLOBAL VARIABLES ---------- #
 global capture              # video capture
 global after_id             # id of the after function
+global face_detector        # detector to detect faces in the image (HOG-based)
+global shape_predictor      # predictor to predict the location of 68 landmarks (points) on the face
 
 # ---------- CUSTOM CLASSES ---------- #
 class Camera():
@@ -42,6 +44,27 @@ class Camera():
         return self.width, self.height
 
 # ---------- CUSTOM FUNCTIONS ---------- #
+def detect_facial_landmarks(img_gray):
+    """
+    Detects the facial landmarks on the image
+    Args:
+        img_gray (_type_): grayscale image
+    Returns:
+        list: list of the facial landmarks
+    """
+    # use the biggest face detected
+    face = max(face_detector(img_gray), key=lambda r: r.area())
+    # predict landmarks of the face using the shape predictor on the grayscale image
+    landmarks = shape_predictor(img_gray, face)
+    # for each one of the 68 landmarks, get coordinates and store them in a list
+    landmark_points_ref = []
+    for p in range(0,68):
+        x = landmarks.part(p).x
+        y = landmarks.part(p).y
+        # cv2.circle(img, (x,y), 3, (255,0,0), -1)                        # show the landmarks on the image
+        landmark_points_ref.append((x,y))
+    return landmark_points_ref
+
 def extract_index_nparray(nparray):
     """
     Extracts the index from a numpy array
@@ -108,6 +131,9 @@ def realtime_face_swap(img):
         img (_type_): image to swap the face with
     """
     global capture
+    global after_id
+    global face_detector
+    global shape_predictor
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)    # the grayscale image has only one channel in 
                                                         # comparison with the color format so it's easier 
                                                         # to process for the CPU
@@ -117,37 +143,48 @@ def realtime_face_swap(img):
     face_detector = dlib.get_frontal_face_detector()
     # import shape predictor to predict the location of 68 landmarks (points) on the face
     shape_predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-
+    # points of the mouth
+    mouth_points = [
+    # [48],  # <outer mouth>
+    # [49],
+    # [50],
+    # [51],
+    # [52],
+    # [53],
+    # [54],
+    # [55],
+    # [56],
+    # [57],
+    # [58],  # </outer mouth>
+    [59],  # <inner mouth>
+    [60],
+    [61],
+    [62],
+    [63],
+    [64],
+    [65],
+    [66],
+    [67],  # </inner mouth>
+]
 
     # 2) FIND LANDMARKS IN THE REFERENCE IMAGE AND CREATE A CONVEX HULL
     try:
-        # face_detector returns a list of rectangles (with top-left and right-bottom corners) that contain the bounding boxes of the faces in the image
-        faces = face_detector(img_gray)
-        # predict landmarks of the face using the shape predictor on the grayscale image
-        landmark_points_ref = []
-        for face in faces:
-            landmarks = shape_predictor(img_gray, face)
-            # for each landmark, get coordinates and store them in a list
-            for p in range(0,68):
-                x = landmarks.part(p).x
-                y = landmarks.part(p).y
-                # cv2.circle(img, (x,y), 3, (255,0,0), -1)                    # show the landmarks on the image
-                landmark_points_ref.append((x,y))
+        # find landmarks of the reference face
+        landmark_points_ref = detect_facial_landmarks(img_gray)
 
         # convexhull -> external boundary of the points (the degrees inside cannot be bigger than 180°)
         # also called "minimum convex polygon"
         np_points_ref = np.array(landmark_points_ref,np.int32)              # convert the list of points to a numpy array
-        convexhull_ref = cv2.convexHull(np_points_ref)                      # get the convex hull of the points
+        convexhull_ref = cv2.convexHull(np_points_ref)                      # create the convex hull
         # cv2.polylines(img, [convexhull], True, (255,0,0), 3)                # show the convex hull on the image
 
-
-        # 3) FACE SEGMENTATION INTO TRIANGLES USING DELAUNAY TRIANGULATION
+    # 3) FACE SEGMENTATION INTO TRIANGLES USING DELAUNAY TRIANGULATION
         rect = cv2.boundingRect(convexhull_ref)                             # get the bounding rectangle of the convex hull
         # (x,y,w,h) = rect                                                    # get the coordinates of the rectangle and draw it on the image
         # cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)
         subdiv = cv2.Subdiv2D(rect)                                         # create a subdiv2D object with the rectangle
         subdiv.insert(landmark_points_ref)                                  # insert the landmark points
-        triangles = subdiv.getTriangleList()                                # get the list of triangles
+        triangles = subdiv.getTriangleList()                                # get the list of Delaunay triangles
         triangles = np.array(triangles, dtype=np.int32)                     # convert the list to a numpy array
 
         # process each triangle
@@ -166,7 +203,7 @@ def realtime_face_swap(img):
             # the condition returns an array with the indexes of the points that satisfy the condition -> which point it might be
             # axis=1 returns the first element
             # the custom function returns only the value of the index
-            index_pt1 = extract_index_nparray(np.where((np_points_ref == pt1).all(axis=1)))    
+            index_pt1 = np.where((np_points_ref == pt1).all(axis=1))[0][0]
             index_pt2 = extract_index_nparray(np.where((np_points_ref == pt2).all(axis=1)))
             index_pt3 = extract_index_nparray(np.where((np_points_ref == pt3).all(axis=1)))
 
@@ -174,19 +211,22 @@ def realtime_face_swap(img):
             if index_pt1 is not None and index_pt2 is not None and index_pt3 is not None:
                 triangle = [index_pt1,index_pt2,index_pt3]
                 triangles_indexes.append(triangle)
-        # 4) SWAPPING LOOP 
+
+        # remove duplicates
+        triangles_indexes = list(set(tuple(t) for t in triangles_indexes))
+
+    # 4) SWAPPING LOOP 
         app.after_cancel(after_id)                                  # stop calling the function
-        swapping_loop(img, face_detector, shape_predictor, landmark_points_ref, triangles_indexes)
-    except:
+        swapping_loop(img, landmark_points_ref, triangles_indexes, mouth_points)
+    except Exception as e:
+        print(e)
         default_camera("No face detected in the selected image")
 
-def swapping_loop(img, face_detector, shape_predictor, landmark_points_ref, triangles_indexes):
+def swapping_loop(img, landmark_points_ref, triangles_indexes, mouth_points):
     """
     Swaps the face in real time
     Args:
         img (_type_): image to swap the face with
-        face_detector (_type_): face detector
-        shape_predictor (_type_): shape predictor
         landmark_points_ref (_type_): landmark points of the reference image
         triangles_indexes (_type_): list of triangles with vertices indexes
     """
@@ -196,40 +236,38 @@ def swapping_loop(img, face_detector, shape_predictor, landmark_points_ref, tria
     frame = cv2.flip(frame,1)                               # flip the frame horizontally
     gray_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)     # convert the frame to grayscale
     new_face = np.zeros_like(frame)                         # create a new image with the same size as the frame
+    mouth_points_set = set(mp[0] for mp in mouth_points)    # convert the list of mouth points to a set
 
-    # 5) GET LANDMARKS FROM THE CURRENT FRAME
-    # process the frame to get the landmarks and convex hull
-    faces = face_detector(gray_frame)
-    landmark_points_frame = []
-    for face in faces:
-        landmarks = shape_predictor(gray_frame, face)
-        for p in range(0,68):
-            x = landmarks.part(p).x
-            y = landmarks.part(p).y
-            landmark_points_frame.append((x,y))
-
-    # 6) APPLY TRIANGLES TO THE CURRENT FRAME IF THERE ARE LANDMARKS
     try:
+    # 5) GET LANDMARKS FROM THE CURRENT FRAME
+        # process the frame to get the landmarks
+        landmark_points_frame = detect_facial_landmarks(gray_frame)
+
+    # 6) APPLY TRIANGLES TO THE CURRENT FRAME IF FACE IS DETECTED
         if (len(landmark_points_frame) != 0):
+            # create a convex hull of the landmarks in the current frame
             np_points_frame = np.array(landmark_points_frame,np.int32)
             convexhull_frame = cv2.convexHull(np_points_frame) 
             # use triangles from the reference image and apply them to the current frame
             for indexes in triangles_indexes:
+                v1 = indexes[0]
+                v2 = indexes[1]
+                v3 = indexes[2]
                 # # SHOW TRIANGLES IN THE CURRENT FRAME
-                # pt1 = landmark_points_frame[indexes[0]]
-                # pt2 = landmark_points_frame[indexes[1]]
-                # pt3 = landmark_points_frame[indexes[2]]
+                # pt1 = landmark_points_frame[v1]
+                # pt2 = landmark_points_frame[v2]
+                # pt3 = landmark_points_frame[v3]
 
                 # # # show the triangles on the frame
                 # cv2.line(frame, pt1, pt2, (255,0,0), 1)
                 # cv2.line(frame, pt2, pt3, (255,0,0), 1)
                 # cv2.line(frame, pt3, pt1, (255,0,0), 1)
             
-                #triangle in the reference image
-                #get vertexes
-                pt1_ref = landmark_points_ref[indexes[0]]
-                pt2_ref = landmark_points_ref[indexes[1]]
-                pt3_ref = landmark_points_ref[indexes[2]]
+                # triangle in the reference image
+                # get vertexes
+                pt1_ref = landmark_points_ref[v1]
+                pt2_ref = landmark_points_ref[v2]
+                pt3_ref = landmark_points_ref[v3]
                 # create an array with the triangle vertexes from the reference image
                 triangle_ref = np.array([pt1_ref,pt2_ref,pt3_ref], np.int32)
                 # get the bounding rectangle of the triangle
@@ -247,30 +285,34 @@ def swapping_loop(img, face_detector, shape_predictor, landmark_points_ref, tria
 
                 # triangle in the current frame
                 # get vertexes
-                pt1_frame = landmark_points_frame[indexes[0]]
-                pt2_frame = landmark_points_frame[indexes[1]]
-                pt3_frame = landmark_points_frame[indexes[2]]
-                #create an array with the triangle vertexes from the current frame
+                pt1_frame = landmark_points_frame[v1]
+                pt2_frame = landmark_points_frame[v2]
+                pt3_frame = landmark_points_frame[v3]
+                # create an array with the triangle vertexes from the current frame
                 triangle_frame = np.array([pt1_frame,pt2_frame,pt3_frame], np.int32)
                 # get the bounding rectangle of the triangle
                 rect_frame = cv2.boundingRect(triangle_frame)
                 # get its coordinates
                 (x_frame,y_frame,w_frame,h_frame) = rect_frame
                 # crop triangle image
-                # cropped_triangle_frame = frame[y_frame: y_frame + h_frame, x_frame: x_frame + w_frame]
+                cropped_triangle_frame = frame[y_frame: y_frame + h_frame, x_frame: x_frame + w_frame]
                 cropped_frame_mask = np.zeros((h_frame, w_frame), np.uint8)
                 points_frame = np.array([[pt1_frame[0] - x_frame, pt1_frame[1] - y_frame],
                                         [pt2_frame[0] - x_frame, pt2_frame[1] - y_frame],
                                         [pt3_frame[0] - x_frame, pt3_frame[1] - y_frame]], np.int32)
                 cv2.fillConvexPoly(cropped_frame_mask, points_frame, 255)
-                # cropped_frame = cv2.bitwise_and(cropped_triangle_frame, cropped_triangle_frame, mask=cropped_frame_mask)
+                cropped_frame = cv2.bitwise_and(cropped_triangle_frame, cropped_triangle_frame, mask=cropped_frame_mask)
                 
-                # warp triangle from the reference image to the current frame
-                points_src = np.float32(points_ref)
-                points_dst = np.float32(points_frame)
-                M = cv2.getAffineTransform(points_src, points_dst)
-                warped_triangle = cv2.warpAffine(cropped_ref, M, (w_frame, h_frame), flags=cv2.INTER_NEAREST)
-                warped_triangle = cv2.bitwise_and(warped_triangle, warped_triangle, mask=cropped_frame_mask)
+                # check if the triangle is in the mouth area - no need to warp it
+                if v1 in mouth_points_set and v2 in mouth_points_set and v3 in mouth_points_set:
+                    warped_triangle = cropped_frame
+                # else warp triangle from the reference image to the current frame
+                else:
+                    points_src = np.float32(points_ref)
+                    points_dst = np.float32(points_frame)
+                    M = cv2.getAffineTransform(points_src, points_dst)
+                    warped_triangle = cv2.warpAffine(cropped_ref, M, (w_frame, h_frame), flags=cv2.INTER_NEAREST)
+                    warped_triangle = cv2.bitwise_and(warped_triangle, warped_triangle, mask=cropped_frame_mask)
 
                 # apply the transformation to the frame
                 area = new_face[y_frame: y_frame + h_frame, x_frame: x_frame + w_frame]             # get the area where the triangle will be applied
@@ -283,7 +325,9 @@ def swapping_loop(img, face_detector, shape_predictor, landmark_points_ref, tria
             # make a mask of the face
             face_mask = np.zeros_like(gray_frame)                               # create a black image the same size of the frame
             head_mask = cv2.fillConvexPoly(face_mask, convexhull_frame, 255)    # fill the face with white
-            face_mask = cv2.bitwise_not(head_mask)                              # invert the mask (face black, background white)
+            convexhull_mouth = cv2.convexHull(np_points_frame[60:])             # get the convex hull of the mouth
+            head_mask = cv2.fillConvexPoly(head_mask, convexhull_mouth, 0)      # fill the mouth with black
+            face_mask = cv2.bitwise_not(head_mask)                              # invert the mask (face and mouth black, background white)
 
             # remove the face from the frame
             head_noface = cv2.bitwise_and(frame, frame, mask=face_mask)
@@ -296,7 +340,9 @@ def swapping_loop(img, face_detector, shape_predictor, landmark_points_ref, tria
             center_face = (int((x + x + w) / 2), int((y + y + h) / 2))
             seamlessclone = cv2.seamlessClone(result, frame, head_mask, center_face, cv2.NORMAL_CLONE)
             frame = seamlessclone
-    except:
+            
+    except Exception as e:
+        print(e)
         pass
         
     show_img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)          # convert the frame to RGBA
@@ -304,7 +350,7 @@ def swapping_loop(img, face_detector, shape_predictor, landmark_points_ref, tria
     photo_image = ImageTk.PhotoImage(image=captured_img)        # convert the frame to Tkinter format
     camera_widget.photo_image = photo_image                     # keep a reference to the image to avoid garbage collection
     camera_widget.configure(image=photo_image)                  # show the image on the label
-    after_id = camera_widget.after(20, lambda: swapping_loop(img, face_detector, shape_predictor, landmark_points_ref, triangles_indexes))  # call this function again in 20 milliseconds
+    after_id = camera_widget.after(20, lambda: swapping_loop(img, landmark_points_ref, triangles_indexes, mouth_points))  # call this function again in 20 milliseconds
 
         
 # ---------- MAIN ------------ #
